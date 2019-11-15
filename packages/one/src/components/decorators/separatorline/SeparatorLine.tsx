fde932f20329a81b05f71d21e4d81330c2854e86
Johan Lindskogen <johan.lindskogen@gmail.com>
2019-11-15 21:14:12 +0100

    Add other repro

--- packages/one/src/components/decorators/separatorline/SeparatorLine.tsx
+++ packages/one/src/components/decorators/separatorline/SeparatorLine.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
+import { faCircle as farCircle } from "@fortawesome/free-regular-svg-icons/faCircle";
+import { faCircle as fasCircle } from "@fortawesome/free-solid-svg-icons/faCircle";
 
 export interface Props {
   text: string;
 }
 
-export const Header: React.FC<Props> = () => <h1>Header</h1>;
+export const Header: React.FC<Props> = () => (
+  <h1>
+    {farCircle} {fasCircle}
+  </h1>
+);
